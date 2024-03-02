f7863f37dfa2b49162d3929f7796b69d7b6dc69c
Betojr04 <betomasia12@gmail.com>
2024-03-01 18:23:12 -0700

    initialized a springboot project StockPriceViewer to practiec Java programming with this simple project, just fetching stock prices from a third party api

---
+++ src/main/java/com/Beto/Stock/StockPriceViewerApplication.java
@@ -0,0 +1,14 @@
+package com.Beto.Stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceViewerApplication.class, args);
+		System.out.println("Hello, Welcome to StockPriceViwer");
+	}
+
+}
