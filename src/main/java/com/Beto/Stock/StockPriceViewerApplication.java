f231bb434ad4c10f126232c54051922aaef102e5
Betojr04 <betomasia12@gmail.com>
2024-03-02 12:16:44 -0700

    added greeting in the program added a prompt for user to input the stock they want to view and a scanner instance to read the input that the user inputs

--- src/main/java/com/Beto/Stock/StockPriceViewerApplication.java
+++ src/main/java/com/Beto/Stock/StockPriceViewerApplication.java
@@ -2,13 +2,21 @@ package com.Beto.Stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class StockPriceViewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockPriceViewerApplication.class, args);
-		System.out.println("Hello, Welcome to StockPriceViwer");
+		System.out.println("Hello, Welcome to StockPriceViwer where you can view stock prices for any stock" +
+				"you wish to see");
+		Scanner scanner  = new Scanner(System.in);
+
+		System.out.println("Please enter the Stock Price you want to see: ");
+		String stock = scanner.nextLine();
+
+
 	}
 
 }
